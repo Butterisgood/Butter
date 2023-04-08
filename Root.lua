17fd8c654747f044e5305f7fb01e95e8fedb5525
Butterisgood <81777090+Butterisgood@users.noreply.github.com>
2023-04-08 10:00:40 -0400

    Add files via upload

---
+++ Root.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 13822889 then -- 🌳 Lumber Tycoon 2
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Butterisgood/Butter/main/lt2'))("")
+elseif game.PlaceId == 6897226634 then -- Timber! 🌲
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Butterisgood/Butter/main/lt2'))("")
+end
