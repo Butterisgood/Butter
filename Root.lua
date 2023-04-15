7824acbc3855b475916daa708335514f6e20b8a3
Butterisgood <81777090+Butterisgood@users.noreply.github.com>
2023-04-15 00:09:08 -0400

    Update Root.lua

--- Root.lua
+++ Root.lua
@@ -2,4 +2,6 @@ if game.PlaceId == 13822889 then -- 🌳 Lumber Tycoon 2
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Butterisgood/Butter/main/lt2'))("")
 elseif game.PlaceId == 6897226634 then -- Timber! 🌲
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Butterisgood/Butter/main/timber-obfuscated.lua'))("")
+elseif game.PlaceId == 537413528 then -- Build A Boat For Treasure
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Butterisgood/Butter/main/btbft.txt'))("")
 end
