d14e42e61e99e32972099e6cebd935441e759e70
Butterisgood <81777090+Butterisgood@users.noreply.github.com>
2023-04-18 03:09:18 -0400

    Update Root.lua

--- Root.lua
+++ Root.lua
@@ -4,4 +4,6 @@ elseif game.PlaceId == 6897226634 then -- Timber! 🌲
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Butterisgood/Butter/main/timber-obfuscated.lua'))("")
 elseif game.PlaceId == 537413528 then -- Build A Boat For Treasure
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Butterisgood/Butter/main/btbft.txt'))("")
+elseif game.PlaceId == 10514822281 then -- Case Opening Simulator 2
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Butterisgood/Butter/main/Case%20Opening%20Simulator%202-obfuscated.lua'))("")
 end
