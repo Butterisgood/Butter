6f7076a64be7155d56bc3302f495db006c959857
Butterisgood <81777090+Butterisgood@users.noreply.github.com>
2023-10-06 19:26:34 -0400

    SpookFinder.lua

--- SpookFinder.lua
+++ SpookFinder.lua
@@ -2,4 +2,5 @@ task.spawn(function()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Butterisgood/Butter/main/SpookFinderob.lua'))("")
 end)
 
+wait(5)
 queue_on_teleport([[loadstring(game:HttpGet('https://raw.githubusercontent.com/Butterisgood/Butter/main/SpookFinder.lua'))("")]])
